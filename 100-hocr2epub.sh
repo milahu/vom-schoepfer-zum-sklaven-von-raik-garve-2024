b5db9511a0676f6d4465dc3945ec0f28fed1c77d
Milan Hauth <milahu@gmail.com>
2025-10-25 16:09:49 +0200

    add 100-hocr2epub.sh

---
+++ 100-hocr2epub.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+dst=$(basename "$0" .sh).epub
+
+doc_title="$(head -n1 readme.md | sed 's/^#\s*//')"
+
+if [ -e "$dst" ]; then
+  echo "error: output exists: $dst"
+  exit 1
+fi
+
+args=(
+  hocr-to-epub-fxl
+  --output "$dst"
+  --scale 0.5
+  --image-format avif
+  --text-format svg
+  --doc-title "$doc_title"
+  *-ocr/*.hocr
+)
+
+"${args[@]}" "$@"
+echo "done $dst"
+
+rm -rf $dst.unzip
+mkdir $dst.unzip
+cd $dst.unzip
+unzip -q ../$dst
+cd ..
+
+echo "done $dst.unzip/index.html"
