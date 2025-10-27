9f0662a7fa26665db12e903a5438267c882e54a7
Milan Hauth <milahu@gmail.com>
2025-10-27 13:45:28 +0100

    100-hocr2epub.sh: use scan_resolution from 030-measure-page-size.txt

--- 100-hocr2epub.sh
+++ 100-hocr2epub.sh
@@ -6,15 +6,24 @@ dst=$(basename "$0" .sh).epub
 
 doc_title="$(head -n1 readme.md | sed 's/^#\s*//')"
 
+if false; then
+  scan_resolution=600
+else
+  source 030-measure-page-size.txt
+fi
+
 if [ -e "$dst" ]; then
   echo "error: output exists: $dst"
   exit 1
 fi
 
+# downscale to 300 dpi
+scale=$(python -c "print(300 / $scan_resolution)")
+
 args=(
   hocr-to-epub-fxl
   --output "$dst"
-  --scale 0.5
+  --scale "$scale"
   --image-format avif
   --text-format svg
   --doc-title "$doc_title"
